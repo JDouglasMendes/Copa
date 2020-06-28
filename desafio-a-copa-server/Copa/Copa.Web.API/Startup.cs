dd5a6916de0e49a65ca7120487b36c5c17680615
Douglas Mendes <douglasaleixomendes@gmail.com>
2020-06-28 14:59:46 -0300

    Adicionando Swagger

--- desafio-a-copa-server/Copa/Copa.Web.API/Startup.cs
+++ desafio-a-copa-server/Copa/Copa.Web.API/Startup.cs
@@ -10,37 +10,59 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
 
 namespace Copa.Web.API
 {
     public class Startup
     {
         public Startup(IConfiguration configuration) => Configuration = configuration;
-        
+
         public IConfiguration Configuration { get; }
-       
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            NativeInjectorBootStrapper.RegisterServices(services);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Desafio Copa",
+                    Version = "v1",
+                    Description = "Desafio a Copa.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Douglas Mendes",
+                        Email = string.Empty,
+                    },
+                });
+            });
+
             services.AddAutoMapperSetup();
             services.AddMemoryCache();
+
+            NativeInjectorBootStrapper.RegisterServices(services);
         }
-       
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {  
+        {
             app.UseRouting();
 
-            app.UseCors(c =>
-            {
-                c.AllowAnyHeader();
-                c.AllowAnyMethod();
-                c.AllowAnyOrigin();
-            });
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Desafio a Copa");
             });
         }
     }
