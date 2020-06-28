a5b11f22464f0aa33b7f85beee2c1421450c6665
Douglas Mendes <douglasaleixomendes@gmail.com>
2020-06-28 14:25:55 -0300

    Ajustes Finais

--- desafio-a-copa-server/Copa/Copa.Domain.Test/Copa/Partidas/RegraVencedorPartidaConsiderandoNumerosNoNomeTest.cs
+++ desafio-a-copa-server/Copa/Copa.Domain.Test/Copa/Partidas/RegraVencedorPartidaConsiderandoNumerosNoNomeTest.cs
@@ -12,13 +12,17 @@ namespace Copa.Domain.Test.Copa.Partidas
         [Theory]
         [InlineData("Equipe 19", "Equipe 100", "Equipe 19")]
         [InlineData("E 1", "A 2", "E 1")]
+        [InlineData("E", "A", null)]
         public void ExecuteRegra(string equipe1, string equipe2, string resultado)
         {
             var regra = new RegraVencedorPartidaConsiderandoNumerosNoNome();
             var eq1 = EquipeBuilder.CrieEquipeRegraNomeAlfanumerico(equipe1);
             var eq2 = EquipeBuilder.CrieEquipeRegraNomeAlfanumerico(equipe2);
             var result = regra.ExecuteRegra(eq1, eq2);
-            Assert.Equal(resultado, result.NomeEquipe.NomeCompleto);
+            if (resultado != null)
+                Assert.Equal(resultado, result.NomeEquipe.NomeCompleto);
+            if (resultado == null && result == null)
+                Assert.True(true);           
         }
     }
 }
